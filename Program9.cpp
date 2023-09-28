1c2eef12bf926ba85b0211280bb439b103b13477
Jhun Mark <jhunmarkromano2@gmaile.com>
2023-09-28 20:22:07 +0800

    test run

---
+++ Program9.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    conts int T = 1;
+    conts int F = 0;
+
+    cout << "value of p: " << T;
+    cout << "Value of !p: " << !T;
+    cout << "Value of P and Q: " << T && T;
+
+    return 0;
+}
